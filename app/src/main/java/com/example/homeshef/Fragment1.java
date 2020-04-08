8363611964367a0428a9f1d94e668fa3e0001667
sgu960323 <wjhappyboy@hanmail.net>
2020-04-08 21:47:28 +0900

    add cook container

--- app/src/main/java/com/example/homeshef/Fragment1.java
+++ app/src/main/java/com/example/homeshef/Fragment1.java
@@ -1,20 +1,24 @@
 package com.example.homeshef;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
 
 public class Fragment1 extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment1, container, false);
+
     }
 }
