6bc9e19e2577d96f9a4e3c8e144853285483b8b8
sgu960323 <wjhappyboy@hanmail.net>
2020-04-08 22:17:36 +0900

    add import

--- app/src/main/java/com/example/homeshef/MainActivity.java
+++ app/src/main/java/com/example/homeshef/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.homeshef.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,7 +18,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<FirebaseAuth> extends AppCompatActivity {
 
     // 비밀번호 정규식
     private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$");
