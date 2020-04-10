d2f926c4e2fd8287fd7a2b9baccf3cb7f83b531e
sgu960323 <wjhappyboy@hanmail.net>
2020-04-10 21:08:59 +0900

    change fragment replace to fragment add

--- app/src/main/java/com/example/homeshef/MenuActivity.java
+++ app/src/main/java/com/example/homeshef/MenuActivity.java
@@ -5,6 +5,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,19 +15,15 @@ public class MenuActivity extends AppCompatActivity {
     Fragment3 fragment3;
     Fragment4 fragment4;
     Fragment5 fragment5;
+
     BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_main);
-
-        fragment1=new Fragment1();
-        fragment2=new Fragment2();
-        fragment3=new Fragment3();
-        fragment4=new Fragment4();
-        fragment5=new Fragment5();
         bottomNavigationView=findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+        fragment3=new Fragment3();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment3).commit();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -34,19 +31,110 @@ public class MenuActivity extends AppCompatActivity {
                 int select=item.getItemId();
                 switch(select){
                     case R.id.menu_cook:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                        if(fragment1==null){
+
+                            fragment1=new Fragment1();
+                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+                        }
+                        if(fragment1!=null){
+                            getSupportFragmentManager().beginTransaction().show(fragment1).commit();
+                        }
+                        if(fragment2!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment2).commit();
+                        }
+                        if(fragment3!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment3).commit();
+                        }
+                        if(fragment4!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
+                        }
+                        if(fragment5!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
+                        }
                         return true;
                     case R.id.menu_user:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+                        if(fragment2==null){
+                            fragment2=new Fragment2();
+                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment2).commit();
+                        }
+                        if(fragment1!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment1).commit();
+                        }
+                        if(fragment2!=null){
+                            getSupportFragmentManager().beginTransaction().show(fragment2).commit();
+                        }
+                        if(fragment3!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment3).commit();
+                        }
+                        if(fragment4!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
+                        }
+                        if(fragment5!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
+                        }
                         return true;
                     case R.id.menu_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+                        if(fragment3==null){
+                            fragment3=new Fragment3();
+                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment3).commit();
+                        }
+                        if(fragment1!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment1).commit();
+                        }
+                        if(fragment2!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment2).commit();
+                        }
+                        if(fragment3!=null){
+                            getSupportFragmentManager().beginTransaction().show(fragment3).commit();
+                        }
+                        if(fragment4!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
+                        }
+                        if(fragment5!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
+                        }
                         return true;
                     case R.id.menu_chat:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment4).commit();
+                        if(fragment4==null){
+                            fragment4=new Fragment4();
+                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment4).commit();
+                        }
+                        if(fragment1!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment1).commit();
+                        }
+                        if(fragment2!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment2).commit();
+                        }
+                        if(fragment3!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment3).commit();
+                        }
+                        if(fragment4!=null){
+                            getSupportFragmentManager().beginTransaction().show(fragment4).commit();
+                        }
+                        if(fragment5!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
+                        }
                         return true;
                     case R.id.menu_mypage:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment5).commit();
+                        if(fragment5==null){
+                            fragment5=new Fragment5();
+                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment5).commit();
+                        }
+                        if(fragment1!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment1).commit();
+                        }
+                        if(fragment2!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment2).commit();
+                        }
+                        if(fragment3!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment3).commit();
+                        }
+                        if(fragment4!=null){
+                            getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
+                        }
+                        if(fragment5!=null){
+                            getSupportFragmentManager().beginTransaction().show(fragment5).commit();
+                        }
                         return true;
                     default:
                         return false;
