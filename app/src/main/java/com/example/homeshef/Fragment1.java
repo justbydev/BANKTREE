eddc35ff2ee25d1f81a8153713ce48936530f7b8
sgu960323 <wjhappyboy@hanmail.net>
2020-04-10 21:37:39 +0900

    change first tab to home

--- app/src/main/java/com/example/homeshef/Fragment1.java
+++ app/src/main/java/com/example/homeshef/Fragment1.java
@@ -22,13 +22,14 @@ public class Fragment1 extends Fragment {
         ViewGroup viewgroup=(ViewGroup)inflater.inflate(R.layout.fragment1, container, false);
         Button button=viewgroup.findViewById(R.id.edit_button);
         final LinearLayout listView=viewgroup.findViewById(R.id.scroll_container);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {//button 클릭시의 리스너
             @Override
             public void onClick(View v) {
-                Context context=getContext().getApplicationContext();
-                LayoutInflater inflater2=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-                inflater2.inflate(R.layout.cook_text, listView, true);
-            }
+                Context context=getContext().getApplicationContext();//context는 activity 전반적 정보 담김
+                //getcontext()는 fragment,adapter,view 위한 것, getapplicationcontext()는 activity위한 것, 여기서는 getSystemService를 사용하기 위해 사용함
+                LayoutInflater inflater2=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);//따로 만든 레이아웃을 메모리에 inflate 하기 위한 것
+                inflater2.inflate(R.layout.cook_text, listView, true);//cook_text layout을 inflater2에 inflate하고 attachtoRoot를 true로 해서
+            }                                                                      //아래에 이어 붙여서 쓸 수 있도록 설정(listView 자리에 쌓기)
         });
         return viewgroup;
     }
--- app/src/main/java/com/example/homeshef/MenuActivity.java
+++ app/src/main/java/com/example/homeshef/MenuActivity.java
@@ -22,23 +22,24 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_main);
         bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.getMenu().getItem(2).setChecked(true);//처음 작동시킬 때 홈 화면 탭 버튼이 체크되도록
         fragment3=new Fragment3();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment3).commit();
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment3).commit();//처음 작동시킬 때 홈 화면 fragment가 보이도록
+        //getSupportFragmentManager를 통해서 쉽게 fragment를 가져올 수 있다
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {//bottomnavigationview의 selected 리스너
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int select=item.getItemId();
                 switch(select){
-                    case R.id.menu_cook:
-                        if(fragment1==null){
+                    case R.id.menu_cook://각각의 하단 탭을 선택할 시, switch~case 문 사용, 5가지 모두 같은 방법을 사용
+                        if(fragment1==null){//아직 생성된 것이 아니면 new Fragment()를 통해서 객체 생성을 해야 하므로
 
                             fragment1=new Fragment1();
-                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
-                        }
-                        if(fragment1!=null){
-                            getSupportFragmentManager().beginTransaction().show(fragment1).commit();
-                        }
+                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();//replace 대신 add를 사용,
+                        }                                                                               //replace는 기존 Activity에 있던 fragment를 완전히 없애고 새로운 것을 얹기에 기존 데이터가 사라지지만
+                        if(fragment1!=null){                                                    //add는 없애지 않고 fragment stack에 쌓아두는 것, 그래서 데이터가 유지 가능
+                            getSupportFragmentManager().beginTransaction().show(fragment1).commit();//add는 각각 fragment마다 한번씩만 해서 stack overflow가 발생하지 않도록 하고 hide,show 사용
+                        }                                                           //쌓아있는 fragment 중에서 보일 것은 show()를 하고 숨길 것은 hide()를 한다
                         if(fragment2!=null){
                             getSupportFragmentManager().beginTransaction().hide(fragment2).commit();
                         }
