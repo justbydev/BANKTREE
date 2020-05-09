f289f6d6a4831c90cedd5e74db3f736214a20544
sgu960323 <wjhappyboy@hanmail.net>
2020-05-09 14:34:11 +0900

    change flow of register

--- app/src/main/java/com/aram/banktree/MainActivity.java
+++ app/src/main/java/com/aram/banktree/MainActivity.java
@@ -24,6 +24,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.w3c.dom.Text;
+
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,15 +33,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //define view objects
     EditText editTextEmail;
     EditText editTextPassword;
+    EditText editTextPassword2;
     EditText editName;
     RadioGroup genderradio;
     DatePicker birthpicker;
     Button buttonSignup;
+    Button validateemailbutton;
     TextView textviewSingin;
     TextView textviewMessage;
+    TextView emailerrortext;
     ProgressDialog progressDialog;
     //define firebase object
     FirebaseAuth firebaseAuth;
+    int emailcheck=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,118 +64,130 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //initializing views
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextPassword2=(EditText)findViewById(R.id.editTextPassword2);
         editName=(EditText)findViewById(R.id.editTextName);
         genderradio=(RadioGroup)findViewById(R.id.genderradio);
         birthpicker=(DatePicker)findViewById(R.id.birthpicker);
         textviewSingin= (TextView) findViewById(R.id.textViewSignin);
         textviewMessage = (TextView) findViewById(R.id.textviewMessage);
+        emailerrortext=(TextView)findViewById(R.id.emailerrortext);
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
+        validateemailbutton=(Button)findViewById(R.id.validateemailbutton);
         progressDialog = new ProgressDialog(this);
 
-        //button click event
-        buttonSignup.setOnClickListener(this);
-        textviewSingin.setOnClickListener(this);
-    }
-
-    //Firebse creating a new user
-    private void registerUser(){
-        //사용자가 입력하는 email, password를 가져온다.
+        //사용자가 입력하는 email, password, name, gender, birth를 가져온다.
         final String email = editTextEmail.getText().toString().trim();
-        final String password = editTextPassword.getText().toString().trim();
+        final String password = editTextPassword.getText().toString();
+        final String repassword=editTextPassword2.getText().toString();
         final String name=editName.getText().toString().trim();
         String gender=null;
+        final String realgender;
         if(genderradio.getCheckedRadioButtonId()!=-1){
             RadioButton rd=(RadioButton)findViewById(genderradio.getCheckedRadioButtonId());
             gender=rd.getText().toString().trim();
         }
+        realgender=gender;
         final int year=birthpicker.getYear();
         final int month=birthpicker.getMonth()+1;
         final int day=birthpicker.getDayOfMonth();
-        //email과 password가 비었는지 아닌지를 체크 한다.
+        validateemailbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailcheck=checkemail(email);
+            }
+        });
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_empty(email, password, repassword, name, realgender)){
+                    if(checkpassword(password, repassword)){
+                        if(emailcheck==1) {
+                            //email과 password가 제대로 입력되어 있다면 계속 진행된다.
+                            progressDialog.setMessage("등록중입니다. 기다려 주세요...");
+                            progressDialog.show();
+                            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if(task.isSuccessful()){
+                                                DatabaseReference memberreference=FirebaseDatabase.getInstance().getReference("Member");
+                                                Member member= new Member();
+                                                member.setEmail(email);
+                                                member.setName(name);
+                                                member.setGender(realgender);
+                                                member.setYear(year);
+                                                member.setMonth(month);
+                                                member.setDay(day);
+                                                memberreference.child("mem").push().setValue(member);
+                                                finish();
+                                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                                            }
+                                            else{
+                                                textviewMessage.setText("서버 에러\n");
+                                            }
+                                            progressDialog.dismiss();
+                                        }
+                                    });
+                        }
+                        else{
+                            textviewMessage.setText("이메일 인증을 받으셔야 합니다.\n");
+                        }
+                    }
+                }
+            }
+        });
+
+        //button click event
+
+        textviewSingin.setOnClickListener(this);
+    }
+    private boolean check_empty(String email, String password, String repassword, String name, String gender){
         if(TextUtils.isEmpty(email)){
             Toast.makeText(this, "Email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
-            return;
+            return false;
         }
         if(TextUtils.isEmpty(password)){
             Toast.makeText(this, "Password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
-            return;
+            return false;
+        }
+        if(TextUtils.isEmpty(repassword)){
+            Toast.makeText(this, "Password를 재입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return false;
         }
         if(TextUtils.isEmpty(name)){
             Toast.makeText(this, "성명을 입력해 주세요.", Toast.LENGTH_SHORT).show();
-            return;
+            return false;
         }
         if(TextUtils.isEmpty(gender)){
             Toast.makeText(this, "성별을 체크해 주세요.", Toast.LENGTH_SHORT).show();
-            return;
+            return false;
         }
-
-        //email과 password가 제대로 입력되어 있다면 계속 진행된다.
-        progressDialog.setMessage("등록중입니다. 기다려 주세요...");
-        progressDialog.show();
-
-        //creating a new user
-
-        final String finalGender = gender;
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            DatabaseReference memberreference= FirebaseDatabase.getInstance().getReference("Member");
-                            Member member=new Member();
-
-                            member.setEmail(email);
-                            member.setName(name);
-                            member.setGender(finalGender);
-                            member.setYear(year);
-                            member.setMonth(month);
-                            member.setDay(day);
-                            memberreference.child("mem").push().setValue(member);
-                            finish();
-                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                        } else {
-                            //에러발생시
-                            if(password.length()<6){
-                                textviewMessage.setText("암호 최소 6자리 이상\n");
-                            }
-                            else{
-                                textviewMessage.setText("서버 에러\n");
-                            }
-                            Toast.makeText(MainActivity.this, "등록 에러!", Toast.LENGTH_SHORT).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
-
+        return true;
     }
-
     //button click event
     @Override
     public void onClick(View view) {
-        if(view == buttonSignup) {
-            //TODO
-            registerUser();
-        }
-
         if(view == textviewSingin) {
             //TODO
             startActivity(new Intent(this, LoginActivity.class)); //추가해 줄 로그인 액티비티
         }
     }
-    private void checkpassword(String password, String repassword){
+    private boolean checkpassword(String password, String repassword){
         if(password.length()<8||!Pattern.matches("^([a-zA-Z0-9])$", password)){
             textviewMessage.setText("비밀번호는 영문자, 숫자 포함 8자리 이상입니다.\n");
-            return;
+            return false;
         }
         if(!password.equals(repassword)){
             textviewMessage.setText("비밀번호가 일치하지 않습니다.\n");
-            return;
+            return false;
         }
+        return true;
     }
-    private void checkemail(String email, FirebaseAuth mAuth){
+    private int checkemail(String email){
         if(!Pattern.matches("^([a-zA-Z0-9]+@[a-zA-Z0-9]+)$", email)){
             textviewMessage.setText("제대로 된 이메일 형식이 아닙니다.\n");
-            return;
+            return 0;
         }
+        return 1;
     }
 }
