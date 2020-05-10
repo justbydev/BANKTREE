f7cd8aca9bf21085ad3011df0dae7646fc7c0489
sgu960323 <wjhappyboy@hanmail.net>
2020-05-10 14:02:15 +0900

    check for same email, validation

---
+++ app/src/main/java/com/aram/banktree/Memberemail.java
@@ -0,0 +1,14 @@
+package com.aram.banktree;
+
+public class Memberemail {
+    String email;
+    public Memberemail(){
+
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+}
