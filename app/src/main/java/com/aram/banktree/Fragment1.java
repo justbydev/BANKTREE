d736ecefaacc3fd1363449d875c79aeb2ee2b817
sgu960323 <wjhappyboy@hanmail.net>
2020-05-10 20:28:15 +0900

    change fragment structure

--- app/src/main/java/com/aram/banktree/Fragment1.java
+++ app/src/main/java/com/aram/banktree/Fragment1.java
@@ -24,40 +24,7 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup viewgroup=(ViewGroup)inflater.inflate(R.layout.fragment1, container, false);
-        Button button=viewgroup.findViewById(R.id.edit_button);
-        Spinner s=viewgroup.findViewById(R.id.filter_spinner);
-        final LinearLayout listView=viewgroup.findViewById(R.id.scroll_container);
-        button.setOnClickListener(new View.OnClickListener() {//button 클릭시의 리스너
-            @Override
-            public void onClick(View v) {
-                Context context=getContext().getApplicationContext();//context는 activity 전반적 정보 담김
-                //getcontext()는 fragment,adapter,view 위한 것, getapplicationcontext()는 activity위한 것, 여기서는 getSystemService를 사용하기 위해 사용함
-                LayoutInflater inflater2=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);//따로 만든 레이아웃을 메모리에 inflate 하기 위한 것
-                inflater2.inflate(R.layout.cook_text, listView, true);//cook_text layout을 inflater2에 inflate하고 attachtoRoot를 true로 해서
-            }                                                                      //아래에 이어 붙여서 쓸 수 있도록 설정(listView 자리에 쌓기)
-        });
-        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch(position){//spinner의 select listener, position에 각각의 index가 담김
-                    case 0://필터 없음
-                        return;
-                    case 1://거리순
-                        return;
-                    case 2://추천순
-                        return;
-                    case 3://가격순
-                        return;
-                    default:
-                        return;
-                }
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
         return viewgroup;
     }
 }
--- app/src/main/java/com/aram/banktree/MenuActivity.java
+++ app/src/main/java/com/aram/banktree/MenuActivity.java
@@ -20,7 +20,6 @@ public class MenuActivity extends AppCompatActivity {
     Fragment2 fragment2;
     Fragment3 fragment3;
     Fragment4 fragment4;
-    Fragment5 fragment5;
     TextView logbutton;
     BottomNavigationView bottomNavigationView;
     FirebaseAuth firebaseAuth;
@@ -45,16 +44,16 @@ public class MenuActivity extends AppCompatActivity {
                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
-        bottomNavigationView.getMenu().getItem(2).setChecked(true);//처음 작동시킬 때 홈 화면 탭 버튼이 체크되도록
-        fragment3=new Fragment3();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment3).commit();//처음 작동시킬 때 홈 화면 fragment가 보이도록
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);//처음 작동시킬 때 홈 화면 탭 버튼이 체크되도록
+        fragment1=new Fragment1();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();//처음 작동시킬 때 홈 화면 fragment가 보이도록
         //getSupportFragmentManager를 통해서 쉽게 fragment를 가져올 수 있다
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {//bottomnavigationview의 selected 리스너
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int select=item.getItemId();
                 switch(select){
-                    case R.id.menu_cook://각각의 하단 탭을 선택할 시, switch~case 문 사용, 5가지 모두 같은 방법을 사용
+                    case R.id.menu_home://각각의 하단 탭을 선택할 시, switch~case 문 사용, 5가지 모두 같은 방법을 사용
                         if(fragment1==null){//아직 생성된 것이 아니면 new Fragment()를 통해서 객체 생성을 해야 하므로
 
                             fragment1=new Fragment1();
@@ -72,11 +71,8 @@ public class MenuActivity extends AppCompatActivity {
                         if(fragment4!=null){
                             getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
                         }
-                        if(fragment5!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
-                        }
                         return true;
-                    case R.id.menu_user:
+                    case R.id.menu_mentor:
                         if(fragment2==null){
                             fragment2=new Fragment2();
                             getSupportFragmentManager().beginTransaction().add(R.id.container, fragment2).commit();
@@ -93,11 +89,8 @@ public class MenuActivity extends AppCompatActivity {
                         if(fragment4!=null){
                             getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
                         }
-                        if(fragment5!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
-                        }
                         return true;
-                    case R.id.menu_home:
+                    case R.id.menu_chat:
                         if(fragment3==null){
                             fragment3=new Fragment3();
                             getSupportFragmentManager().beginTransaction().add(R.id.container, fragment3).commit();
@@ -114,11 +107,8 @@ public class MenuActivity extends AppCompatActivity {
                         if(fragment4!=null){
                             getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
                         }
-                        if(fragment5!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
-                        }
                         return true;
-                    case R.id.menu_chat:
+                    case R.id.menu_mypage:
                         if(fragment4==null){
                             fragment4=new Fragment4();
                             getSupportFragmentManager().beginTransaction().add(R.id.container, fragment4).commit();
@@ -134,30 +124,6 @@ public class MenuActivity extends AppCompatActivity {
                         }
                         if(fragment4!=null){
                             getSupportFragmentManager().beginTransaction().show(fragment4).commit();
-                        }
-                        if(fragment5!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment5).commit();
-                        }
-                        return true;
-                    case R.id.menu_mypage:
-                        if(fragment5==null){
-                            fragment5=new Fragment5();
-                            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment5).commit();
-                        }
-                        if(fragment1!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment1).commit();
-                        }
-                        if(fragment2!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment2).commit();
-                        }
-                        if(fragment3!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment3).commit();
-                        }
-                        if(fragment4!=null){
-                            getSupportFragmentManager().beginTransaction().hide(fragment4).commit();
-                        }
-                        if(fragment5!=null){
-                            getSupportFragmentManager().beginTransaction().show(fragment5).commit();
                         }
                         return true;
                     default:
