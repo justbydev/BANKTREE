b7326681b65617e8b41bd561aaaa60894c995a8d
sgu960323 <wjhappyboy@hanmail.net>
2020-05-11 14:38:05 +0900

    add colorpicker library & activate color pick button

--- app/src/main/java/com/aram/banktree/Newbook.java
+++ app/src/main/java/com/aram/banktree/Newbook.java
@@ -1,13 +1,18 @@
 package com.aram.banktree;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class Newbook extends AppCompatActivity {
     Button color;
@@ -20,6 +25,7 @@ public class Newbook extends AppCompatActivity {
     Button share;
     EditText book_title;
     EditText book_content;
+    int contentdefaultcolor;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +40,8 @@ public class Newbook extends AppCompatActivity {
         share=(Button)findViewById(R.id.share);
         book_title=(EditText)findViewById(R.id.book_title);
         book_content=(EditText)findViewById(R.id.book_content);
+
+        contentdefaultcolor= ContextCompat.getColor(this, R.color.design_default_color_secondary_variant);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,5 +49,27 @@ public class Newbook extends AppCompatActivity {
                 finish();
             }
         });
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });
     }
+    public void openColorPicker(){
+        AmbilWarnaDialog colorPicker=new AmbilWarnaDialog(this, contentdefaultcolor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                contentdefaultcolor=color;
+                book_content.setBackgroundColor(contentdefaultcolor);
+            }
+        });
+        colorPicker.show();
+    }
+
 }
