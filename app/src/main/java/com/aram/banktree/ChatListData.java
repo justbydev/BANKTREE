dc09ebfb3a30972fa3839285addd6d9cff0a5a1e
sgu960323 <wjhappyboy@hanmail.net>
2020-05-21 10:21:42 +0900

    add chat button & change function

---
+++ app/src/main/java/com/aram/banktree/ChatListData.java
@@ -0,0 +1,15 @@
+package com.aram.banktree;
+
+public class ChatListData {
+    private String nickname;
+    public ChatListData(){};
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+}
