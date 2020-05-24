5793d347b980f302c79dd2406c00a7a5c164b3e2
sgu960323 <wjhappyboy@hanmail.net>
2020-05-24 11:36:53 +0900

    make meesage recyclerview

--- app/src/main/java/com/aram/banktree/EachChat.java
+++ app/src/main/java/com/aram/banktree/EachChat.java
@@ -2,19 +2,35 @@ package com.aram.banktree;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class EachChat extends AppCompatActivity {
     TextView want;
     EditText message_edit;
     Button send_message;
-    RecyclerView message_recycler_view;
+    String me;
+    String other;
+    private RecyclerView message_recycler_view;
+    private EachChatAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth firebaseAuth;
+
+    private ArrayList<ChatMessage> mList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +42,37 @@ public class EachChat extends AppCompatActivity {
 
         want=(TextView)findViewById(R.id.want);
         Intent intent=getIntent();
-        want.setText(intent.getExtras().getString("want"));
+        other=intent.getExtras().getString("want");
+        want.setText(other);
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+        me=firebaseAuth.getCurrentUser().getEmail();
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        message_recycler_view.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        message_recycler_view.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mList=new ArrayList<>();
+        mAdapter = new EachChatAdapter(mList);
+        message_recycler_view.setAdapter(mAdapter);
+
+        send_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text=message_edit.getText().toString();
+                ChatMessage chatMessage=new ChatMessage();
+                chatMessage.setName(me);
+                chatMessage.setText(text);
+                mList.add(chatMessage);
+                mAdapter.notifyDataSetChanged();
+                message_edit.setText("");
+            }
+        });
 
     }
 }
--- app/src/main/java/com/aram/banktree/EachChatAdapter.java
+++ app/src/main/java/com/aram/banktree/EachChatAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,20 +18,37 @@ public class EachChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         this.mList=mList;
     }
 
-    @NonNull
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView message_user;
+        public TextView message_content;
+        public MyViewHolder(View v) {
+            super(v);
+            message_user = v.findViewById(R.id.message_user);
+            message_content=v.findViewById(R.id.message_content);
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context=parent.getContext();
-        LayoutInflater inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v=inflater.inflate(R.layout.eachchat, parent, false);
-        //EachChatAdapter eachChatAdapter=new EachChatAdapter(v);
-        //return eachChatAdapter;
-        return null;
+    public EachChatAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.messageitem, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        MyViewHolder myViewHolder=(MyViewHolder)holder;
+        myViewHolder.message_user.setText(mList.get(position).getName());
+        myViewHolder.message_content.setText(mList.get(position).getText());
     }
 
     @Override
