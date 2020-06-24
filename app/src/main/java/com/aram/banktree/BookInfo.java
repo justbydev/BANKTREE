d5b18c55eb8057b1b81fda38f0c47a8102bf78ef
BonJunKu <ku96kbj@naver.com>
2020-06-24 22:37:52 +0900

    infoconnect1

--- app/src/main/java/com/aram/banktree/BookInfo.java
+++ app/src/main/java/com/aram/banktree/BookInfo.java
@@ -5,7 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BookInfo extends AppCompatActivity {
+    public class Item {
+        private int item_image;
+        private String item_title;
 
+        public Item(int item_image, String item_title) {
+            this.item_image = item_image;
+            this.item_title = item_title;
+        }
+
+        public String getItem_title() {
+            return item_title;
+        }
+        public int getItem_image() {
+            return item_image;
+        }
+
+        public void setItem_title(String item_title) {
+            this.item_title = item_title;
+        }
+        public void setItem_image(int item_image) {
+            this.item_image = item_image;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
